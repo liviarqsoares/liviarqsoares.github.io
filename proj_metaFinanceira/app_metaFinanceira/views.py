1f0abbc0c5976b5de97669331e3de74e93f6bd1a
Edilson Torres <edilsontor77@gmail.com>
2023-10-13 22:00:23 -0300

    Exibindo value das variaveis

--- proj_metaFinanceira/app_metaFinanceira/views.py
+++ proj_metaFinanceira/app_metaFinanceira/views.py
@@ -7,4 +7,4 @@ def calcular(request):
     n1 = int(request.POST.get('meta',0))
     n2 = int(request.POST.get('valorMensal',0))
     resultado = n1 / n2
-    return render(request, 'home.html', {'resultado': resultado})
+    return render(request, 'home.html', {'resultado': resultado, 'n1': n1, 'n2': n2})
