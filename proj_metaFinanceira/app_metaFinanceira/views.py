c934a24994747f50622fa0c73c0831271d36f034
Edilson Torres <edilsontor77@gmail.com>
2023-10-15 00:31:27 -0300

    Add funcoes de formatacao de entradas e saidas

--- proj_metaFinanceira/app_metaFinanceira/views.py
+++ proj_metaFinanceira/app_metaFinanceira/views.py
@@ -4,7 +4,27 @@ def home(request):
     return render(request, 'home.html')
 
 def calcular(request):
-    n1 = int(request.POST.get('meta',0))
-    n2 = int(request.POST.get('valorMensal',0))
-    resultado = n1 / n2
-    return render(request, 'home.html', {'resultado': resultado, 'n1': n1, 'n2': n2})
+    meta = float(request.POST.get('meta',0))
+    valorMensal = float(request.POST.get('valorMensal',0))
+    resultado = int(meta / valorMensal)
+
+    return render(request, 'home.html', {'resultado': formatarResultado(resultado), 'meta': formatarEntradas(meta),
+                                          'valorMensal': formatarEntradas(valorMensal)})
+
+def formatarEntradas(n):
+    numero = f'{n:_.2f}'
+    resultado = numero.replace('.', ',').replace('_', '.')
+    return resultado
+
+def formatarResultado(resultado):
+    if(resultado > 12):
+        r = resultado / 12
+        resposta = f'{round(r)} Anos'
+        return resposta
+    
+    if(resultado == 12):
+        return  f'1 Ano'
+        
+    else:
+        resposta = f'{resultado} Meses'
+        return resposta
