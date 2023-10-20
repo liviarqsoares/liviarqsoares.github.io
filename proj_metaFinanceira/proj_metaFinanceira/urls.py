cb96c908b24d3fc1cc3b6e0345eaf1605392fe3d
liviarqsoares <89165406+liviarqsoares@users.noreply.github.com>
2023-10-20 18:04:12 -0300

    Delete proj_metaFinanceira/proj_metaFinanceira directory

--- proj_metaFinanceira/proj_metaFinanceira/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app_metaFinanceira import views
-
-urlpatterns = [
-    # configuração das rotas
-    path('', views.home, name='home'),
-    path('calcular', views.calcular, name='calcular'),
-]
